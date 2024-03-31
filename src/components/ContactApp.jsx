e30077a77c6951e21e1ac3ba4311c3f0831b8f74
Vina Nur Fauziah <@vinaanurfauziah@gmail.com>
2024-03-31 07:59:39 +0700

    latihan studi kasus: membuat fitur registrasi

--- src/components/ContactApp.jsx
+++ src/components/ContactApp.jsx
@@ -3,11 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 import Navigation from './Navigation';
 import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
- 
+import RegisterPage from '../pages/RegisterPage';
+
 class ContactApp extends React.Component {
   constructor(props) {
     super(props);
- 
+
     this.state = {
       authedUser: null,
     };
@@ -22,29 +23,29 @@ class ContactApp extends React.Component {
           </header>
           <main>
             <Routes>
-              <Route path="/*" element={<p>Halaman Login</p>} />
-              <Route path="/register" element={<p>Halaman Register</p>} />
+              <Route path='/*' element={<p>Halaman Login</p>} />
+              <Route path='/register' element={<RegisterPage />} />
             </Routes>
           </main>
         </div>
-      )
+      );
     }
-    
+
     return (
-      <div className="contact-app">
+      <div className='contact-app'>
         <header className='contact-app__header'>
           <h1>Aplikasi Kontak</h1>
           <Navigation />
         </header>
         <main>
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/add" element={<AddPage />} />
+            <Route path='/' element={<HomePage />} />
+            <Route path='/add' element={<AddPage />} />
           </Routes>
         </main>
       </div>
     );
   }
 }
- 
+
 export default ContactApp;
